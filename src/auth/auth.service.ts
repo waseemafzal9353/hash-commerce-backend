ea5b7417bcc99f6bd94e39aca68159b26493ac5f
waseemafzal9353 <waseem.afzal9353@gmail.com>
2023-07-31 14:48:47 -0400

    throw error like pro

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/DTOs/user.dto';
-
+import { BusinessException } from 'src/Exceptions/business.exception';
 @Injectable()
 export class AuthService {
-    
-    async createUser (createUserDto: CreateUserDto){
+
+    async createUser(createUserDto: CreateUserDto) {
+        throw new BusinessException(
+            'users',                                 // Error domain
+            `User with was not found.`, // Internal message
+            'User not found',                        // API message
+            HttpStatus.NOT_FOUND,
+        )
         console.log("createUserDto", createUserDto)
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { CustomExceptionFilter } from './Exceptions/exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(8080);
+
 }
 bootstrap();
